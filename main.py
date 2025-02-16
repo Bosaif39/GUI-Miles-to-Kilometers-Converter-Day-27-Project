3b5287b0f7e6848aee3e3cf2d63e75d6e0bb2225
Abdullah Saif <bosaif39tm@gmail.com>
2025-02-16 23:36:08 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ window = Tk()
 window.title("Mile to Km Converter")
 window.config(padx=50,pady=50)
 
-# is equal to
+# is equal to label
 my_label= Label(font=("Arial",14,"bold"))
 my_label.grid(column=0,row=1)
 my_label.config(text="is equal to")
 
 
-# Miles
+# Miles label
 my_label2= Label(font=("Arial",14,"bold"))
 my_label2.grid(column=3,row=0)
 my_label2.config(text="Miles")
 
 
-# Km
+# Km label
 my_label3= Label(font=("Arial",14,"bold"))
 my_label3.grid(column=3,row=1)
 my_label3.config(text="Km")
@@ -27,7 +27,7 @@ Results= Label(font=("Arial",14,"bold"))
 Results.grid(column=1,row=1)
 Results.config(text=0,padx=50,pady=50)
 
-
+# Function to convert miles to kilometers
 def calculate_miles():
     miles = float(inputEntery.get())
     kilometers = miles * 1.60934
@@ -41,8 +41,6 @@ button.grid(column=1,row=3)
 
 inputEntery = Entry(width=10)
 inputEntery.grid(column=1, row=0)
-
-
 
 
 window.mainloop()
